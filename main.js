29182b05884fac2f664fb7a4413b30f2a80a418f
Mohd <azeemsre1@gmail.com>
2022-12-05 16:58:36 +0530

    ExpanseTracker with LocalStorage

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var form=
-`<div class="container">
+var form = `<div class="container">
         
 
 <div class="">
@@ -37,34 +36,49 @@ var form=
   
 `;
 //  add form to htnl
-document.getElementById('form').innerHTML=form
-
+document.getElementById("form").innerHTML = form;
 
 //initialize to trap data
-let details=[];
-table()
+let details = [];
+getData();
+table();
 
-// to save data function
-function save(){
-    let expense=document.getElementById('expense');
-    let e=document.getElementById('catagory');
-    let catagory=e.options[e.selectedIndex].text;
-    let description=document.getElementById('description')
-    let data={
-        expense:expense.value,
-        catagory:catagory,
-        description:description.value
-    }
-    details.push(data)
-    console.log(details);
-    table()
+// to get data from local storage
+function getData() {
+  let data = localStorage.getItem("details");
+  if (data) {
+    details = JSON.parse(data);
+  } else {
+    setData();
+  }
 }
 
+// to send data to local storage
+function setData() {
+  localStorage.setItem("details", JSON.stringify(details));
+}
 
+// to save data function
+function save() {
+  let expense = document.getElementById("expense");
+  let e = document.getElementById("catagory");
+  let catagory = e.options[e.selectedIndex].text;
+  let description = document.getElementById("description");
+  if(expense.value==0) return alert('Expense is Empty')
+  let data = {
+    expense: expense.value,
+    catagory: catagory,
+    description: description.value,
+  };
+  details.push(data);
+  setData();
+  // console.log(details);
+  table();
+}
 
 // Add detals table
-function table(){
-    let table=`<table class="table">
+function table() {
+  let table = `<table class="table">
     <thead>
       <tr>
         <th scope="col">#</th>
@@ -77,19 +91,90 @@ function table(){
       </tr>
     </thead>
     <tbody>`;
-    for(let i=0;i<details.length;i++){
-        table=table+`<tr>
-        <th scope="row">${i+1}</th>
+  for (let i = 0; i < details.length; i++) {
+    table =
+      table +
+      `<tr>
+        <th scope="row">${i + 1}</th>
         <td>${details[i].expense}</td>
         <td>${details[i].catagory}</td>
         <td>${details[i].description}</td>
-        <td><button type='button' class='btn btn-warning'>Edit</button></td>
-        <td><button type='button' class='btn btn-warning'>Delete</button></td>
-      </tr>`
-    }
-    table=table+`</tbody>
+        <td><button type='button' class='btn btn-warning' onclick='edit(${i})'>Edit</button></td>
+        <td><button type='button' class='btn btn-danger' onclick='deleteData(${i})'>Delete</button></td>
+      </tr>`;
+  }
+  table =
+    table +
+    `</tbody>
     </table>`;
-    document.getElementById('table').innerHTML=table;
+  let expense = document.getElementById("expense");
+  let description = document.getElementById("description");
+  // expense.value='';
+  // description.value='';
+  document.getElementById("form").innerHTML = form;
+  document.getElementById("table").innerHTML = table;
 }
 
+// edit function
+function edit(index) {
+  var editForm = `<div class="container">
+        
 
+  <div class="">
+    <label for="inputEmail4" class="form-label">Choose Expense</label>
+    <div class="input-group mb-3">
+    <span class="input-group-text">$</span>
+    <input type="number" class="form-control" id='newexpense' value=${details[index].expense} aria-label="Dollar amount (with dot and two decimal places)" placeholder='Enter Amount'>
+    
+  </div>
+    </div> 
+  
+    
+    <div class='chooseExpense'>
+    <label for="inputEmail4" class="form-label">Choose Catagory</label>
+  <select class="form-select col" id='newcatagory' value=${details[index].catagory} aria-label=".form-select-sm example">
+  <option >Movie</option>
+  <option >Shopping</option>
+  <option >Rent</option>
+  <option >Grocery</option>
+  </select>
+    </div>
+  
+    </div>
+  
+  
+    <div class="secondDiv">
+    <div class="col">
+    <label for="inputPassword4" class="form-label">Add Short Description</label>
+      <input type="text" class="form-control" id='newdescription' value=${details[index].description} placeholder="Description" aria-label="Last name">
+    </div>
+    <button class="btn btn-primary mt-4 btn1" type="submit" onclick='update(${index})'>Update</button>
+  
+  
+    </div>
+    `;
+  document.getElementById("form").innerHTML = editForm;
+}
+
+// update function
+function update(index) {
+  let newexpense = document.getElementById("newexpense");
+  let e = document.getElementById("newcatagory");
+  let newcatagory = e.options[e.selectedIndex].text;
+  let newdescription = document.getElementById("newdescription");
+  details[index] = {
+    expense: newexpense.value,
+    catagory: newcatagory,
+    description: newdescription.value,
+  };
+  setData();
+  table();
+  document.getElementById("form").innerHTML = form;
+}
+
+// delete function
+function deleteData(index) {
+  details.splice(index, 1);
+  setData();
+  table();
+}
